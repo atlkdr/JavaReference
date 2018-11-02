4e08a80b529f33dd9a1f5fb65fde7e42598032c4
Arunaksha Talukdar <36398596+atlkdr@users.noreply.github.com>
2018-11-02 22:03:50 +0530

    Basic's Updated

--- BASIC.java
+++ BASIC.java
@@ -12,12 +12,24 @@ public class Main{
     }
 
   }
+  public static void cal(int... Arr){
+    for(int i=1;i<Arr.length;i++)
+    Arr[0]+=Arr[i];
+    System.out.println("Sum is:"+String.valueOf(Arr[0]));
+  }
   // Static Methord can be called without the requirement of Object Formation 
   public static void main(String args[]){
     // Integer : Wrapper class for int allows functions 
     int x=10_000;
     switch_FUNC(x);
-
+    // Jagged Array
+    int[][] Arr={{1,2,3},{1,2},{1}};
+    for(int i=0;i<Arr.length;i++){
+      for(int j=0;j<Arr[i].length;j++)
+      System.out.print(Arr[i][j]+" ");
+      System.out.println();
+    }
+    cal(1,2,3,4);
     return;
   }
 }
